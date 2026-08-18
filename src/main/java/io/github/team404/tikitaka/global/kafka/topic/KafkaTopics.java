c7edd5e9359bebe330e4b26f03b0cd97a8686d27
junhyung001 <junhyung8325@gmail.com>
2026-08-18 16:13:52 +0900

    feat: Kafka 예약 이벤트 및 토픽 정의

---
+++ src/main/java/io/github/team404/tikitaka/global/kafka/topic/KafkaTopics.java
@@ -0,0 +1,9 @@
+package io.github.team404.tikitaka.global.kafka.topic;
+
+public final class KafkaTopics {
+
+    public static final String RESERVATION_EVENTS = "reservation-events";
+
+    private KafkaTopics() {
+    }
+}
